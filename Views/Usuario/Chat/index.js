c365f8efca580d26940c6e91e132b8cf43d7fc63
Natali <nanatkim11@gmail.com>
2019-11-09 21:57:27 -0300

    layout do chat

--- Views/Usuario/Chat/index.js
+++ Views/Usuario/Chat/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import {List, Colors, Divider} from 'react-native-paper';
 import Header from '../Header';
 
 export default class Chat extends Component {
@@ -11,8 +12,28 @@ export default class Chat extends Component {
   render() {
     return (
       <>
-        <View style={{flex: 1}}>
-          <Text> FAZER O CHAT</Text>
+        <View style={{flex: 1, padding: 10}}>
+          <List.Section>
+            <List.Subheader>Chat</List.Subheader>
+            <List.Item
+              title="Jubileu"
+              description="Uma mensagem bem top dessa pessoa..."
+              left={() => <List.Icon icon="person" />}
+            />
+            <Divider />
+            <List.Item
+              title="Fulaninho"
+              description="Mensagenzinha aqui"
+              left={() => <List.Icon color="blue" icon="person" />}
+            />
+            <Divider />
+            <List.Item
+              title="Teste"
+              description="Digitando..."
+              left={() => <List.Icon icon="person" />}
+            />
+            <Divider />
+          </List.Section>
         </View>
         <Header mudarRota={rota => this.mudarRota(rota)} selected={1} />
       </>
